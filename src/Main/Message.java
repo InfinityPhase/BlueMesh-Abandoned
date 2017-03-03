9ee2b4c78d5882402b77c86e0c051a9fd26711e3
infinity <infinity@ArchOverAll.attlocal.net>
2017-03-03 00:26:37 -0800

    Added Message.java to hold message related stuff, but not sending or recieving

--- src/Main/Message.java
+++ src/Main/Message.java
@@ -1,9 +1,21 @@
+package Main;
+
 import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.json.simple.*;
+import java.text.SimpleDateFormat;
 
 public class Message {
+	SimpleDateFormat time = new SimpleDateFormat("yyyy MM dd HH:mm aaa");
+		// Will output time like: "2017 03 03 01:18.AM"
+	
 	public static string writeMessage(String messageIn, String meshID) {
-		String messageFile = null;
-				
-		return messageFile;
+		Map mappedMessage = new LinkedHashMap();
+		mappedMessage.put("UserID", UserId.getUserUUID());
+		mappedMessage.put("MeshID", meshID);
+		mappedMessage.put("Timestamp", time.toString());
+		mappedMessage.put("Message", messageIn);
+		return String JSONMessage = JSONValue.toJSONString(mappedMessage);
 	}
 }
