7a6028e2edb6f65824d5fec3a40db58928511e8f
infinity <infinity@ArchOverAll.attlocal.net>
2017-03-03 10:29:52 -0800

    Update Message.java

--- src/Main/Message.java
+++ src/Main/Message.java
@@ -4,24 +4,29 @@ import java.util.Map;
 import org.json.simple.*;
 import java.text.SimpleDateFormat;
 
+
+
 public class Message {
-	SimpleDateFormat time = new SimpleDateFormat("yyyy MM dd HH:mm aaa");
-	MessageDigest 256Hash = MessageDigest.getInstance("SHA-256");
+	//MessageDigest 256Hash = MessageDigest.getInstance("SHA-256");
 		// Will output time like: "2017 03 03 01:18.AM"
 	
-	public static string writeMessage(String messageIn, String meshID) throws IOException {
+	public static String writeMessage(String messageIn, String meshID) {
+		SimpleDateFormat time = new SimpleDateFormat("yyyy MM dd HH:mm aaa");
+
 		Map mappedMessage = new LinkedHashMap();
-		mappedMessage.put("UserID", UserId.getUserUUID());
+		mappedMessage.put("UserID", UserID.getUserUUID());
 		mappedMessage.put("MeshID", meshID);
 		mappedMessage.put("Timestamp", time.toString());
-		mappedMessage.put("Hash", hashMessage());
+		//mappedMessage.put("Hash", hashMessage());
 		mappedMessage.put("Message", messageIn);
-		return String JSONMessage = JSONValue.toJSONString(mappedMessage);
+		
+		String JSONMessage = JSONValue.toJSONString(mappedMessage);
+		return JSONMessage;
 	}
-	
+	/*
 	public static string hashMessage(String messageIn) {
 		256Hash.update(messageIn.getBytes("UTF-8"));
 		byte[] messageHash = messageIn.digest();
 		return messageHash;
-	}
+	}*/
 }
